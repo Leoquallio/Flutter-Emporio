078e91f04d6645ad56a37045045631467d7b914f
DanielPetroni <daniel.petroni25@gmail.com>
2022-04-26 15:44:06 -0300

    Revert "refactor home screen"
    This reverts commit be9e5735df4036813b930e9bf6074b2cc0bcf54d.

--- lib/src/product/product_screen.dart
+++ lib/src/product/product_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_emporio/src/themes/colors_app.dart';
 
 class ProductScreen extends StatefulWidget {
   const ProductScreen({Key? key}) : super(key: key);
@@ -11,15 +10,6 @@ class ProductScreen extends StatefulWidget {
 class _ProductScreenState extends State<ProductScreen> {
   @override
   Widget build(BuildContext context) {
-    Size size = MediaQuery.of(context).size;
-    return Expanded(
-      child: Center(
-        child: Container(
-          color: ColorsApp.kSecondColor.withOpacity(0.5),
-          height: size.height * 0.8,
-          width: size.width <= 1000 ? size.width * 0.8 : size.width * 0.6,
-        ),
-      ),
-    );
+    return Container();
   }
 }
