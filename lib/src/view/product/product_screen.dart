278c212dd02891190cbb691ed3056960d1fbfcae
DanielPetroni <daniel.petroni25@gmail.com>
2022-04-27 15:57:06 -0300

    created widget CustomButtomCircular

--- lib/src/view/product/product_screen.dart
+++ lib/src/view/product/product_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_emporio/src/model/produto_model.dart';
 import 'package:flutter_emporio/src/themes/colors_app.dart';
+import 'package:flutter_emporio/src/view/widgets/custom_button_circular.dart';
 
 class ProductScreen extends StatefulWidget {
   const ProductScreen({Key? key}) : super(key: key);
@@ -33,100 +34,97 @@ class _ProductScreenState extends State<ProductScreen> {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return Expanded(
-      child: Center(
-        child: Container(
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(30),
-            color: ColorsApp.kSecondColor.withOpacity(0.5),
-          ),
-          height: size.height * 0.8,
-          width: size.width <= 1000 ? size.width * 0.95 : size.width * 0.6,
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Center(
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(30),
+                color: ColorsApp.kSecondColor.withOpacity(0.5),
+              ),
+              height: size.height * 0.8,
+              width: size.width <= 1000 ? size.width * 0.95 : size.width * 0.6,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Container(
-                      width: size.width * 0.3,
-                      decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(30)),
-                      child: const Padding(
-                        padding: EdgeInsets.all(8.0),
-                        child: TextField(
-                          decoration: InputDecoration(
-                              border: InputBorder.none,
-                              prefixIcon: Icon(Icons.search)),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Container(
+                          width: size.width * 0.3,
+                          decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(30)),
+                          child: const Padding(
+                            padding: EdgeInsets.all(8.0),
+                            child: TextField(
+                              decoration: InputDecoration(
+                                  border: InputBorder.none,
+                                  prefixIcon: Icon(Icons.search)),
+                            ),
+                          ),
                         ),
-                      ),
+                      ],
                     ),
+                    SizedBox(height: size.height * 0.05),
+                    DataTable(
+                        columnSpacing: size.width <= 1000
+                            ? size.width * 0.07
+                            : size.width * 0.1,
+                        columns: const [
+                          DataColumn(label: Text("Id")),
+                          DataColumn(label: Text("Nome")),
+                          DataColumn(label: Text("Valor")),
+                          DataColumn(label: Text("Quantidade ")),
+                          DataColumn(label: Text("")),
+                        ],
+                        rows: products
+                            .map((product) => DataRow(
+                                  cells: [
+                                    DataCell(
+                                      Text(product.idProduto != null
+                                          ? product.idProduto.toString()
+                                          : ''),
+                                    ),
+                                    DataCell(
+                                      Text(product.nomeProduto != null
+                                          ? product.nomeProduto!
+                                          : ''),
+                                    ),
+                                    DataCell(
+                                      Text(product.valorProduto != null
+                                          ? 'R\$ ${product.idProduto}'
+                                          : ''),
+                                    ),
+                                    DataCell(
+                                      Text(product.qtdEstoque != null
+                                          ? product.idProduto.toString()
+                                          : ''),
+                                    ),
+                                    const DataCell(
+                                        Icon(Icons.arrow_forward_ios_outlined)),
+                                  ],
+                                ))
+                            .toList()),
                   ],
                 ),
-                SizedBox(height: size.height * 0.05),
-                DataTable(
-                    columnSpacing: size.width <= 1000
-                        ? size.width * 0.07
-                        : size.width * 0.1,
-                    columns: const [
-                      DataColumn(label: Text("Id")),
-                      DataColumn(label: Text("Nome")),
-                      DataColumn(label: Text("Valor")),
-                      DataColumn(label: Text("Quantidade ")),
-                      DataColumn(label: Text("")),
-                    ],
-                    rows: products
-                        .map((product) => DataRow(
-                              cells: [
-                                DataCell(
-                                  Text(product.idProduto != null
-                                      ? product.idProduto.toString()
-                                      : ''),
-                                ),
-                                DataCell(
-                                  Text(product.nomeProduto != null
-                                      ? product.nomeProduto!
-                                      : ''),
-                                ),
-                                DataCell(
-                                  Text(product.valorProduto != null
-                                      ? 'R\$ ${product.idProduto}'
-                                      : ''),
-                                ),
-                                DataCell(
-                                  Text(product.qtdEstoque != null
-                                      ? product.idProduto.toString()
-                                      : ''),
-                                ),
-                                const DataCell(
-                                    Icon(Icons.arrow_forward_ios_outlined)),
-                              ],
-                            ))
-                        .toList()),
-                // ListView.builder(
-                //   shrinkWrap: true,
-                //   itemCount: products.length,
-                //   itemBuilder: (context, index) => Card(
-                //     child: Row(
-                //         mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                //         children: [
-                //           Text(products[index].nomeProduto!),
-                //           Text(products[index].valorProduto != null
-                //               ? 'R\$ ${products[index].valorProduto.toString()}'
-                //               : ''),
-                //           Text(products[index].idProduto!.toString()),
-                //           GestureDetector(
-                //             child: const Icon(Icons.arrow_forward_ios_sharp),
-                //           )
-                //         ]),
-                //   ),
-                // )
-              ],
+              ),
             ),
           ),
-        ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Padding(
+                padding: EdgeInsets.only(right: size.width * 0.02),
+                child: CustomButtonCircular(
+                    size.height * 0.05, "Adicionar produto", () {}),
+              )
+            ],
+          )
+        ],
       ),
     );
   }
---
+++ lib/src/view/widgets/custom_button_circular.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_emporio/src/themes/colors_app.dart';
+
+class CustomButtonCircular extends StatelessWidget {
+  final double height;
+  final String textButton;
+  final Function onTap;
+  const CustomButtonCircular(this.height, this.textButton, this.onTap,
+      {Key? key})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () => onTap(),
+      child: Container(
+        height: height,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(30),
+            color: ColorsApp.kPrimaryColor),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Center(
+            child: Text(
+              textButton,
+              style: const TextStyle(color: Colors.white),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
