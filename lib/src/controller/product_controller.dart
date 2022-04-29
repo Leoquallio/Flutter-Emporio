67b44cca31f52b96e8271a0cebda71f0f8ad9eaf
DanielPetroni <daniel.petroni25@gmail.com>
2022-04-29 09:40:37 -0300

    created product controller

--- lib/src/controller/product_controller.dart
+++ lib/src/controller/product_controller.dart
@@ -1,33 +1,29 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter_emporio/src/repository.dart/product_repository.dart';
 
 import '../model/produto_model.dart';
 
 class ProductController extends ChangeNotifier {
+  ProductRepository _productRepository;
   List<Produto> products = [];
   bool isLoading = false;
+
+  ProductController(this._productRepository);
 
   Future<void> getAllProducts() async {
     isLoading = true;
     notifyListeners();
-    await Future.delayed(const Duration(seconds: 4));
-    products = [
-      Produto(
-          idProduto: 2,
-          nomeProduto: "Coca",
-          valorProduto: 5.52,
-          dataValidadeProduto: DateTime.now().toString(),
-          descricaoProduto: "Gostoso",
-          gtinProduto: "514747",
-          qtdEstoque: 5),
-      Produto(
-          idProduto: 5,
-          valorProduto: 10.00,
-          nomeProduto: "Coca Cola",
-          dataValidadeProduto: DateTime.now().toString(),
-          descricaoProduto: "Gostoso",
-          gtinProduto: "54454545",
-          qtdEstoque: 2)
-    ];
+    try {
+      Response response = await _productRepository.getAllProducts();
+      if (response.data.runtimeType == List) {
+        products = (response.data as List)
+            .map((product) => Produto.fromJson(product))
+            .toList();
+      }
+    } catch (e) {
+      debugPrint(e.toString());
+    }
     isLoading = false;
     notifyListeners();
   }
